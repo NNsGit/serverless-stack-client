4a491268d82f73fc705a3b0378a1fe0245dfa5db
Nizar Noorani <nizar.noorani@alchemysystems.com>
2020-08-21 12:42:21 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "slsnotes"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://w9pjcmkumi.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_OvrB0uTxs",
+    APP_CLIENT_ID: "1sbdaqhgtfq4cc3ksmpgu3cdt9",
+    IDENTITY_POOL_ID: "us-east-2:64504bb0-1ca7-429c-90c4-1f8678fb99a4"
+  }
+};
